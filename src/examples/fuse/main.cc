1a73fb5147ead9a0817ba35767a0c7431aaddad4
Marcin Sulikowski <marcin.sulikowski@skytechnology.pl>
2014-07-03 16:50:51 +0200

    examples: Implement 0.2.0 version in the fuse client
    Add statfs, access, open, read, flush, and release callbacks to the fuse
    client.
    
    Change-Id: Id7a790f56b319e9f0d6b329b43f609a8a8f715e3

--- src/examples/fuse/main.cc
+++ src/examples/fuse/main.cc
@@ -36,20 +36,19 @@
 
 using namespace polonaise;
 
+static std::string gHostname;
+static int gPort;
 static boost::shared_ptr<apache::thrift::transport::TTransport> gThriftTransport;
 static boost::shared_ptr<PolonaiseClient> gPolonaiseClient;
 static SessionId gSessionId;
 
-bool polonaise_init(const char* hostname) {
+bool polonaise_init() {
 	using namespace apache::thrift;
 	using namespace apache::thrift::protocol;
 	using namespace apache::thrift::transport;
-	if (hostname == nullptr) {
-		hostname = "localhost";
-	}
 
 	try {
-		boost::shared_ptr<TTransport> socket(new TSocket(hostname, 9090));
+		boost::shared_ptr<TTransport> socket(new TSocket(gHostname, gPort));
 		gThriftTransport = boost::make_shared<TBufferedTransport>(socket);
 		boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(gThriftTransport));
 		gPolonaiseClient = boost::make_shared<PolonaiseClient>(protocol);
@@ -70,6 +69,8 @@ void polonaise_term() {
 	}
 }
 
+// FUSE to Polonaise conversions
+
 Descriptor get_descriptor(struct fuse_file_info *fi) {
 	return fi ? static_cast<Descriptor>(fi->fh) : g_polonaise_constants.kNullDescriptor;
 }
@@ -84,6 +85,42 @@ Context get_context(fuse_req_t& req) {
 	ret.umask = ctx->umask;
 	return ret;
 }
+
+int get_open_flags(int posixFlags) {
+	static std::pair<int, int> mappings[] = {
+			{O_RDWR,   OpenFlags::kRead |  OpenFlags::kWrite},
+			{O_RDONLY, OpenFlags::kRead},
+			{O_WRONLY, OpenFlags::kWrite},
+			{O_CREAT,  OpenFlags::kCreate},
+			{O_EXCL,   OpenFlags::kExclusive},
+			{O_APPEND, OpenFlags::kAppend},
+			{O_TRUNC,  OpenFlags::kTrunc},
+	};
+	int polonaiseFlags = 0;
+	for (const auto& mapping : mappings) {
+		if (posixFlags & mapping.first) {
+			polonaiseFlags |= mapping.second;
+		}
+	}
+	return polonaiseFlags;
+}
+
+int get_access_mask(int posixMask) {
+	static std::pair<int, int> mappings[] = {
+			{001, AccessMask::kExecute},
+			{002, AccessMask::kWrite},
+			{004, AccessMask::kRead},
+	};
+	int polonaiseMask = 0;
+	for (const auto& mapping : mappings) {
+		if (posixMask & mapping.first) {
+			polonaiseMask |= mapping.second;
+		}
+	}
+	return polonaiseMask;
+}
+
+// Polonaise to FUSE conversions
 
 struct stat make_stbuf(const FileStat& fs) {
 	struct stat stbuf{};
@@ -139,16 +176,64 @@ fuse_entry_param make_fuse_entry_param(const EntryReply& entry) {
 	return fep;
 }
 
+int make_error_number(StatusCode::type status) {
+	switch(status) {
+		case StatusCode::kE2BIG: return E2BIG;
+		case StatusCode::kEACCES: return EACCES;
+		case StatusCode::kEAGAIN: return EAGAIN;
+		case StatusCode::kEBADF: return EBADF;
+		case StatusCode::kEBUSY: return EBUSY;
+		case StatusCode::kECHILD: return ECHILD;
+		case StatusCode::kEDOM: return EDOM;
+		case StatusCode::kEEXIST: return EEXIST;
+		case StatusCode::kEFAULT: return EFAULT;
+		case StatusCode::kEFBIG: return EFBIG;
+		case StatusCode::kEINTR: return EINTR;
+		case StatusCode::kEINVAL: return EINVAL;
+		case StatusCode::kEIO: return EIO;
+		case StatusCode::kEISDIR: return EISDIR;
+		case StatusCode::kEMFILE: return EMFILE;
+		case StatusCode::kEMLINK: return EMLINK;
+		case StatusCode::kENAMETOOLONG: return ENAMETOOLONG;
+		case StatusCode::kENFILE: return ENFILE;
+		case StatusCode::kENODATA: return ENODATA;
+		case StatusCode::kENODEV: return ENODEV;
+		case StatusCode::kENOENT: return ENOENT;
+		case StatusCode::kENOEXEC: return ENOEXEC;
+		case StatusCode::kENOMEM: return ENOMEM;
+		case StatusCode::kENOSPC: return ENOSPC;
+		case StatusCode::kENOSYS: return ENOSYS;
+		case StatusCode::kENOTBLK: return ENOTBLK;
+		case StatusCode::kENOTDIR: return ENOTDIR;
+		case StatusCode::kENOTEMPTY: return ENOTEMPTY;
+		case StatusCode::kENOTTY: return ENOTTY;
+		case StatusCode::kENXIO: return ENXIO;
+		case StatusCode::kEPERM: return EPERM;
+		case StatusCode::kEPIPE: return EPIPE;
+		case StatusCode::kERANGE: return ERANGE;
+		case StatusCode::kEROFS: return EROFS;
+		case StatusCode::kESPIPE: return ESPIPE;
+		case StatusCode::kESRCH: return ESRCH;
+		case StatusCode::kETIMEDOUT: return ETIMEDOUT;
+		case StatusCode::kETXTBSY: return ETXTBSY;
+		case StatusCode::kEXDEV: return EXDEV;
+	}
+	return EIO;
+}
+
+// Implementation of FUSE callbacks
+
 #define HANDLE_POLONAISE_EXCEPTION_BEGIN try {
 #define HANDLE_POLONAISE_EXCEPTION_END \
 	} catch (Status& s) { \
-		fuse_reply_err(req, s.statusCode); \
+		fuse_reply_err(req, make_error_number(s.statusCode)); \
 	} catch (Failure& f) { \
 		fprintf(stderr, "failure: %s\n", f.message.c_str()); \
 		fuse_reply_err(req, EIO); \
 	} catch (apache::thrift::TException& e) { \
 		fprintf(stderr, "connection error: %s\n", e.what()); \
 		fuse_reply_err(req, EIO); \
+		polonaise_init(); \
 	}
 
 void do_getattr(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi) {
@@ -219,8 +304,86 @@ void do_lookup(fuse_req_t req, fuse_ino_t parent, const char *name) {
 	HANDLE_POLONAISE_EXCEPTION_END
 }
 
+void do_access(fuse_req_t req, fuse_ino_t ino, int mask) {
+	HANDLE_POLONAISE_EXCEPTION_BEGIN
+
+	gPolonaiseClient->access(get_context(req), ino, get_access_mask(mask));
+	fuse_reply_err(req, 0);
+
+	HANDLE_POLONAISE_EXCEPTION_END
+}
+
+void do_open(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi) {
+	HANDLE_POLONAISE_EXCEPTION_BEGIN
+
+	OpenReply reply;
+	gPolonaiseClient->open(reply, get_context(req), ino, get_open_flags(fi->flags));
+	fi->direct_io = reply.directIo;
+	fi->keep_cache = reply.keepCache;
+	fi->nonseekable = reply.nonSeekable;
+	fi->fh = static_cast<uint64_t>(reply.descriptor);
+	if (fuse_reply_open(req, fi) == -ENONET) {
+		gPolonaiseClient->release(get_context(req), ino, reply.descriptor);
+		fi->fh = static_cast<uint64_t>(-2);
+	}
+
+	HANDLE_POLONAISE_EXCEPTION_END
+}
+
+void do_read(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off, struct fuse_file_info *fi) {
+	HANDLE_POLONAISE_EXCEPTION_BEGIN
+
+	std::string buffer;
+	gPolonaiseClient->read(buffer, get_context(req), ino, off, size, get_descriptor(fi));
+	fuse_reply_buf(req, buffer.data(), buffer.size());
+
+	HANDLE_POLONAISE_EXCEPTION_END
+}
+
+void do_flush(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi) {
+	HANDLE_POLONAISE_EXCEPTION_BEGIN
+
+	gPolonaiseClient->flush(get_context(req), ino, get_descriptor(fi));
+	fuse_reply_err(req, 0);
+
+	HANDLE_POLONAISE_EXCEPTION_END
+}
+
+void do_release(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi) {
+	HANDLE_POLONAISE_EXCEPTION_BEGIN
+
+	gPolonaiseClient->release(get_context(req), ino, get_descriptor(fi));
+	fi->fh = static_cast<uint64_t>(-2);
+	fuse_reply_err(req, 0);
+
+	HANDLE_POLONAISE_EXCEPTION_END
+}
+
+void do_statfs(fuse_req_t req, fuse_ino_t ino) {
+	HANDLE_POLONAISE_EXCEPTION_BEGIN
+
+	StatFsReply reply;
+	gPolonaiseClient->statfs(reply, get_context(req), ino);
+	struct statvfs s;
+	s.f_fsid = reply.filesystemId;
+	s.f_namemax = reply.maxNameLength;
+	s.f_frsize = s.f_bsize = reply.blockSize;
+	s.f_blocks = reply.totalBlocks;
+	s.f_bfree = reply.freeBlocks;
+	s.f_bavail = reply.availableBlocks;
+	s.f_files = reply.totalFiles;
+	s.f_ffree = reply.freeFiles;
+	s.f_favail = reply.availableFiles;
+	fuse_reply_statfs(req, &s);
+
+	HANDLE_POLONAISE_EXCEPTION_END
+}
+
+
 int main(int argc, char** argv) {
-	if (!polonaise_init(getenv("POLONAISE_SERVER"))) {
+	gHostname = (getenv("POLONAISE_SERVER") ? getenv("POLONAISE_SERVER") : "localhost");
+	gPort = (getenv("POLONAISE_PORT") ? atoi(getenv("POLONAISE_PORT")) : 9090);
+	if (!polonaise_init()) {
 		fprintf(stderr, "Server connection failed.\n");
 		return 1;
 	}
@@ -230,6 +393,12 @@ int main(int argc, char** argv) {
 	ops.readdir = do_readdir;
 	ops.releasedir = do_releasedir;
 	ops.lookup = do_lookup;
+	ops.access = do_access;
+	ops.open = do_open;
+	ops.read = do_read;
+	ops.flush = do_flush;
+	ops.release = do_release;
+	ops.statfs = do_statfs;
 	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
 	char *mp;
 	if (fuse_parse_cmdline(&args, &mp, NULL, NULL) < 0) {
